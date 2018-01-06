05bdc05100a11e7bd26b459b20fe2df6e6615e9e
Hayden Schoen <haydenschoen@fb.com>
2018-01-05 17:26:10 -0800

    macro!

---
+++ .dep/obj_haymakr_lufa_common_avr_eeconfig.o.d
@@ -0,0 +1,7 @@
+obj_haymakr_lufa/common/avr/eeconfig.o: \
+ ../../tmk_core/common/avr/eeconfig.c config.h \
+ ../../tmk_core/common/eeconfig.h
+
+config.h:
+
+../../tmk_core/common/eeconfig.h:
--- keymap_haymakr.c
+++ keymap_haymakr.c
@@ -8,9 +8,9 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          MPLY, LCTL,LALT,LGUI,FN0, SPC, LEFT,DOWN,RGHT,SPC, FN1, RGUI,RALT,RCTL,EQL),
 
   KEYMAP(TRNS, F1,  F2,  F3,  F4,  F5,                 F6,  F7,  F8,  F9,  F10, TRNS, \
-         TRNS, TRNS,TRNS,TRNS,TRNS,TRNS,               TRNS,TRNS,TRNS,FN8, FN9, TRNS, \
+         TRNS, FN15,TRNS,TRNS,TRNS,TRNS,               TRNS,TRNS,TRNS,FN8, FN9, TRNS, \
          TRNS, TRNS,TRNS,TRNS,TRNS,TRNS,FN4,      FN5, TRNS,MINS,EQL, FN10,FN11,BSLS, \
-         TRNS, TRNS,TRNS,TRNS,TRNS,TRNS,     VOLU,     TRNS,FN13, FN14, LBRC,RBRC,FN12, \
+         TRNS, TRNS,TRNS,TRNS,TRNS,TRNS,     VOLU,     TRNS,FN13,FN14,LBRC,RBRC,FN12, \
          TRNS, TRNS,TRNS,TRNS,TRNS,TRNS,MPRV,VOLD,MNXT,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS),
 
   KEYMAP(TRNS, TRNS,TRNS,TRNS,TRNS,TRNS,               TRNS,7,   8,   9,   PSLS,TRNS, \
@@ -19,6 +19,17 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          TRNS, TRNS,TRNS,TRNS,TRNS,TRNS,     VOLU,     TRNS,TRNS,0,   PDOT,PPLS,PENT, \
          TRNS, TRNS,TRNS,TRNS,TRNS,TRNS,MPRV,VOLD,MNXT,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS),
 };
+
+const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
+{
+    switch (id) {
+        case 0:
+            return (record->event.pressed ?
+                    MACRO( D(LSFT), T(S), U(LSFT), T(U), T(P), T(E), T(R), D(LSFT), T(P), U(LSFT), T(0), T(W), T(E), T(R), D(LSFT), T(M), U(LSFT), T(A), T(C), T(H), T(I), T(N), T(E), T(R), T(Y), END ) :
+                    MACRO_NONE );
+    }
+    return MACRO_NONE;
+}
 
 const action_t PROGMEM fn_actions[] = {
   [0] = ACTION_LAYER_MOMENTARY(1),
@@ -36,4 +47,7 @@ const action_t PROGMEM fn_actions[] = {
   [12] = ACTION_MODS_KEY(MOD_LSFT, KC_BSLS),
   [13] = ACTION_MODS_KEY(MOD_LSFT, KC_MINS),
   [14] = ACTION_MODS_KEY(MOD_LSFT, KC_EQL),
+  [15] = ACTION_MACRO(0),
 };
+
+
