1fb367e53b7665dcfd6d6c6a21eaaa31fc70d700
Hayden Schoen <haydenschoen@fb.com>
2018-01-08 15:54:03 -0800

    new layer pressing raise and lower

--- keymap_haymakr.c
+++ keymap_haymakr.c
@@ -8,16 +8,22 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          MPLY, LCTL,LALT,LGUI,FN0, SPC, LEFT,DOWN,RGHT,SPC, FN1, RGUI,RALT,RCTL,EQL),
 
   KEYMAP(TRNS, F1,  F2,  F3,  F4,  F5,                 F6,  F7,  F8,  F9,  F10, TRNS, \
-         TRNS, FN15,TRNS,TRNS,TRNS,TRNS,               TRNS,TRNS,TRNS,FN8, FN9, TRNS, \
-         TRNS, TRNS,TRNS,TRNS,TRNS,TRNS,FN4,      FN5, TRNS,MINS,EQL, FN10,FN11,BSLS, \
-         TRNS, TRNS,TRNS,TRNS,TRNS,TRNS,     VOLU,     TRNS,FN13,FN14,LBRC,RBRC,FN12, \
-         SLEP, WAKE,TRNS,TRNS,TRNS,TRNS,MPRV,VOLD,MNXT,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS),
+         TRNS, TRNS,TRNS,TRNS,WH_U,TRNS,               TRNS,TRNS,TRNS,FN8, FN9, TRNS, \
+         TRNS, TRNS,FN20,FN21,FN22,FN23,FN4,      FN5, TRNS,MINS,EQL, FN10,FN11,BSLS, \
+         TRNS, TRNS,TRNS,TRNS,WH_D,TRNS,     VOLU,     TRNS,FN13,FN14,LBRC,RBRC,FN12, \
+         TRNS, WAKE,TRNS,TRNS,TRNS,TRNS,MPRV,VOLD,MNXT,TRNS,FN15,TRNS,TRNS,TRNS,TRNS),
 
-  KEYMAP(TRNS, TRNS,TRNS,TRNS,TRNS,TRNS,               TRNS,7,   8,   9,   PSLS,TRNS, \
-         TRNS, TRNS,TRNS,TRNS,TRNS,TRNS,               TRNS,4,   5,   6,   PAST,TRNS, \
-         TRNS, TRNS,TRNS,TRNS,TRNS,TRNS,FN6,      FN7, TRNS,1,   2,   3,   PMNS,PEQL, \
-         TRNS, TRNS,TRNS,TRNS,TRNS,TRNS,     VOLU,     TRNS,TRNS,0,   PDOT,PPLS,PENT, \
-         TRNS, TRNS,TRNS,TRNS,TRNS,TRNS,MPRV,VOLD,MNXT,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS),
+  KEYMAP(TRNS, TRNS,TRNS,TRNS,TRNS,TRNS,               TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+         TRNS, TRNS,TRNS,TRNS,TRNS,TRNS,               TRNS,7,   8,   9,   PSLS,TRNS, \
+         TRNS, TRNS,TRNS,TRNS,TRNS,TRNS,FN6,      FN7, TRNS,4,   5,   6,   PAST,TRNS, \
+         TRNS, TRNS,TRNS,TRNS,TRNS,TRNS,     VOLU,     TRNS,1,   2,   3,   PMNS,PEQL, \
+         TRNS, TRNS,TRNS,TRNS,FN15,TRNS,MPRV,VOLD,MNXT,TRNS,TRNS,0,   PDOT,PPLS,PENT),
+
+  KEYMAP(TRNS, TRNS,TRNS,TRNS,TRNS,TRNS,               TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+         TRNS, FN16,FN17,FN18,FN19,TRNS,               TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+         TRNS, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+         TRNS, TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS,     TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+         TRNS, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS),
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
@@ -25,7 +31,19 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
     switch (id) {
         case 0:
             return (record->event.pressed ?
+                    MACRO( D(LSFT), T(D), U(LSFT), T(E), T(L), D(LSFT), T(B), U(LSFT), T(A), T(B), T(U), T(1), END ) :
+                    MACRO_NONE );
+        case 1:
+            return (record->event.pressed ?
                     MACRO( D(LSFT), T(S), U(LSFT), T(U), T(P), T(E), T(R), D(LSFT), T(P), U(LSFT), T(0), T(W), T(E), T(R), D(LSFT), T(M), U(LSFT), T(A), T(C), T(H), T(I), T(N), T(E), T(R), T(Y), END ) :
+                    MACRO_NONE );
+        case 2:
+            return (record->event.pressed ?
+                    MACRO( D(LSFT), T(I), U(LSFT), T(M), T(4), T(K), T(N), T(I), T(F), T(3), D(LSFT), T(C), U(LSFT), T(U), T(7), D(LSFT), T(C), U(LSFT), T(U), T(7), END ) :
+                    MACRO_NONE );
+        case 3:
+            return (record->event.pressed ?
+                    MACRO( D(LSFT), T(S), U(LSFT), T(U), T(L), T(T), T(4), T(N), D(LSFT), T(O), U(LSFT), T(F), D(LSFT), T(S), U(LSFT), T(W), T(E), T(E), T(T), T(N), T(E), T(S), T(S), END ) :
                     MACRO_NONE );
     }
     return MACRO_NONE;
@@ -47,7 +65,17 @@ const action_t PROGMEM fn_actions[] = {
   [12] = ACTION_MODS_KEY(MOD_LSFT, KC_BSLS),
   [13] = ACTION_MODS_KEY(MOD_LSFT, KC_MINS),
   [14] = ACTION_MODS_KEY(MOD_LSFT, KC_EQL),
-  [15] = ACTION_MACRO(0),
+  [15] = ACTION_LAYER_MOMENTARY(3),
+  [16] = ACTION_MACRO(0),
+  [17] = ACTION_MACRO(1),
+  [18] = ACTION_MACRO(2),
+  [19] = ACTION_MACRO(3),
+  [20] = ACTION_MODS_KEY(MOD_LSFT | MOD_LGUI, KC_LEFT),
+  [21] = ACTION_MODS_KEY(MOD_LSFT | MOD_LALT, KC_LEFT),
+  [22] = ACTION_MODS_KEY(MOD_LSFT | MOD_LALT, KC_RGHT),
+  [23] = ACTION_MODS_KEY(MOD_LSFT | MOD_LGUI, KC_RGHT),
 };
+
+
 
 
